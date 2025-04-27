f480ece818e3266cc30909d5f0c2251df552806f
Moustafa Moussa <moustafamoussa@192.168.69.172>
2025-04-27 18:20:06 +0300

    Change server port to 3001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
